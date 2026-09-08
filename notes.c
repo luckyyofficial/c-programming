9f48a1c288259112867472de53e00be526edf4e9
Lucky <lakshayjofficial@gmail.com>
2026-09-08 20:27:46 +0530

    C programming

--- notes.c
+++ notes.c
@@ -3,6 +3,9 @@ int main (){
 
     // Notes for C programming language
 
+    // C is one of the oldest and most powerful programming languages. 
+    // It is known for its speed, efficiency, and close interaction with computer hardware.
+
 
 
     return 0;
