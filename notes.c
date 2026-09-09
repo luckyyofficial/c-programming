ccfe329907e0553f52a48a3f4645f42a73bbdc92
Lucky <lakshayjofficial@gmail.com>
2026-09-09 07:53:48 +0530

    C programming

--- notes.c
+++ notes.c
@@ -6,6 +6,21 @@ int main (){
     // C is one of the oldest and most powerful programming languages. 
     // It is known for its speed, efficiency, and close interaction with computer hardware.
 
+    // Variables
+    
+    // A variable is a named container that stores a value in memory.
+
+    // There are 3 types of variable in c programming ,
+
+    // int 
+    int a = 3;       // Used to store integer numbers 
+    
+    // float
+    float b = 4.7;   // Used to store float numbers 
+    
+    // float
+    char c = 'A';    // Used to store alphabets
+
 
 
     return 0;
