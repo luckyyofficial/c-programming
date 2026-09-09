9fa2a9609d8584a3fb7bea28b35bb254704cb8ed
Lucky <lakshayjofficial@gmail.com>
2026-09-09 07:54:12 +0530

    C programming

--- notes.c
+++ notes.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main (){
-
+ 
     // Notes for C programming language
-
+ 
     // C is one of the oldest and most powerful programming languages. 
     // It is known for its speed, efficiency, and close interaction with computer hardware.
 
